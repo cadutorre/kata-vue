50764eb41d54dac0fee085a626096f6130bff3e5
Carlos Eduardo Torre <cadutorre@gmail.com>
2019-05-03 01:31:30 -0300

    Antes do font-awesome

---
+++ src/components/Corretor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-input-group
+      prepend="Username"
+      class="mt-3"
+    >
+      <b-form-input></b-form-input>
+      <b-input-group-append>
+        <b-button variant="info">Button</b-button>
+      </b-input-group-append>
+    </b-input-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'corretor',
+  data () {
+    return {
+      name: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
